5ea47ecacbdc6bf3b6e3b78f422bf2601eb15bb0
Anadroid <anajelicic89@gmail.com>
2021-05-05 12:57:29 +0200

    Changing button styles for all the buttons

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -9,12 +9,12 @@ const ToggleButton = ({
   defaultLabel,
   pressedLabel,
   ariaLabel,
-  toggle = false,
+  toggle = false
 }) => {
   const style = classnames(css.root, className);
   const states = {
     default: defaultLabel ? defaultLabel : 'off',
-    pressed: pressedLabel ? pressedLabel : 'on',
+    pressed: pressedLabel ? pressedLabel : 'on'
   };
 
   const toggleButton = (e) => {
@@ -28,13 +28,14 @@ const ToggleButton = ({
       className={style}
       onClick={(e) => toggleButton(e)}>
       <div>
-        <span className={classnames({ [css.pressed]: !toggle })}>
+        <span className={classnames({ [css.pressedText]: toggle })}>
           {states.default}
         </span>
-        <span className={classnames({ [css.pressed]: toggle })}>
+        <span className={classnames({ [css.pressedText]: !toggle })}>
           {states.pressed}
         </span>
       </div>
+      <div className={classnames(css.pressed, css[`slide${toggle ? 0 : 1}`])} />
     </button>
   );
 };
